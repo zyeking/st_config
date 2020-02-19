1526c712bf7b72d346a83127cbfc71d02424618b
zyeking <zyeking@163.com>
2020-02-19 22:32:19 +0800

    scroll

--- config.h
+++ config.h
@@ -190,6 +190,11 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
+	{ MODKEY ,              XK_j,     kscrollup,            {.i =  1} },
+	{ MODKEY ,              XK_k,   kscrolldown,            {.i =  1} },
+	{ MODKEY|ControlMask ,  XK_j,     kscrollup,            {.i = -1} },
+	{ MODKEY|ControlMask ,  XK_k,   kscrolldown,            {.i = -1} },
+	
 };
 
 /*
